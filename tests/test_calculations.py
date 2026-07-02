dbf0661bbb4df438a8e7540148c96de67f61dd65
Kaamya Shinde <kaams@equinor.com>
2026-07-02 11:08:25 +0200

    linting updates

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,12 +1,14 @@
 from geo_calculator.calculations import find_average, gardners_equation, inverse_gardners_equation
 import pytest
 
+
 def test_find_average_given_list_of_numbers():
     numbers = [1, 2, 3, 4, 5, 6]
 
     result = find_average(numbers)
 
     assert result == 3.5
+
 
 def test_gardners_equation() -> None:
     velocity = 2000  # m/s
@@ -15,19 +17,16 @@ def test_gardners_equation() -> None:
     # By default, approx considers numbers within a relative tolerance of 1e-6
     assert gardners_equation(velocity) == pytest.approx(expected_density)
 
+
 def test_inverse_gardners_equation() -> None:
     density = 2.0730949  # g/cm3
     expected_velocity = 2000  # m/s
 
     assert inverse_gardners_equation(density) == pytest.approx(expected_velocity)
 
-    assert inverse_gardners_equation(
-        gardners_equation(expected_velocity)
-    ) == pytest.approx(expected_velocity)
+    assert inverse_gardners_equation(gardners_equation(expected_velocity)) == pytest.approx(expected_velocity)
 
-    assert gardners_equation(inverse_gardners_equation(density)) == pytest.approx(
-        density
-    )
+    assert gardners_equation(inverse_gardners_equation(density)) == pytest.approx(density)
 
 
 def test_gardners_equation_negative_velocity() -> None:
