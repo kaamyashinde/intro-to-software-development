bf78063de1b50c01004a72ea3eabf6e7a69929e5
Kaamya Shinde <kaams@equinor.com>
2026-07-02 11:02:35 +0200

    implemen tgardner equation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,2 +1,17 @@
 def find_average(numbers):
+    """Calculate the average of some numbers
+    Args:
+        numbers: A sequence of numbers
+    Returns:
+        The average value of the numbers
+    """
     return sum(numbers) / len(numbers)
+
+def gardners_equation(velocity):
+    """Calculate the density of a material using the Gardners equation
+    Args:
+        velocity: The velocity of the material
+    Returns:
+        The density of the material
+    """
+    return 0.31 * velocity ** 0.25
