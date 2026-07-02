1476855e63ad29ea469acaf5f7b34500a682fea0
Kaamya Shinde <kaams@equinor.com>
2026-07-02 11:02:35 +0200

    implemen tgardner equation

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,5 @@
-from geo_calculator.calculations import find_average
-
+from geo_calculator.calculations import find_average, gardners_equation
+import pytest
 
 def test_find_average_given_list_of_numbers():
     numbers = [1, 2, 3, 4, 5, 6]
@@ -7,3 +7,10 @@ def test_find_average_given_list_of_numbers():
     result = find_average(numbers)
 
     assert result == 3.5
+
+def test_gardners_equation() -> None:
+    velocity = 2000  # m/s
+    expected_density = 2.0730949  # g/cm3
+
+    # By default, approx considers numbers within a relative tolerance of 1e-6
+    assert gardners_equation(velocity) == pytest.approx(expected_density)
